3f47edb3384f6870beb5b27a32517385cf4a51e7
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-08-20 10:00:30 -0300

    adicionando mui

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Grid, Typography,  } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Grid>
+      <Typography variant="h1">Hello World</Typography>
+    </Grid>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: "sans-serif",
+    },
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        h1: {
+          color: "red",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
