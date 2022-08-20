3cc82ccdf8a0544073e7ff90047063e3600de6fc
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-08-20 10:39:09 -0300

    pagina de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,68 @@
-import { Grid, Typography,  } from "@mui/material";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Box, Button, TextField, Grid } from "@mui/material";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn, isAuthenticated } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  }
+
   return (
-    <Grid>
-      <Typography variant="h1">Hello World</Typography>
-    </Grid>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      width="100vw"
+    >
+      <Grid container xs={4}>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            label="Digite seu e-mail"
+            type="email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            label="Digite sua senha"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            fullWidth
+            disabled={!password || !email}
+          >
+            Entrar
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
