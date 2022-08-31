7bcec73b8adf01f4fe822a9e8c842c1a6d3661f4
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-08-30 23:25:12 -0300

    responsividade do modal

--- src/shared/components/ModalDetalhesFilme/index.tsx
+++ src/shared/components/ModalDetalhesFilme/index.tsx
@@ -34,68 +34,52 @@ export function ModalDetalhesFilme({ movie, open }: ModalDetalhesFilmeProps) {
           alt={"poster do filme" + movie.title}
           src={movie.banner_path}
           width="600px"
-          height="200px"
+          height="240px"
           style={{ borderRadius: "8px 8px 0px 0px" }}
         />
         <Grid container px={2} py={1}>
-          <Grid container item xs={8}>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Typography variant="h5" fontWeight="bold" mr={1}>
+          <Grid item xs={12} sm={8}>
+            <Box display="flex" alignItems="center">
+              <Typography variant="h5" fontWeight="bold" mr={3}>
                 {movie.title}
               </Typography>
               <Typography variant="body1">
                 {new Date(movie.release_date).getFullYear()}
               </Typography>
             </Box>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                mr={2}
-              >
-                <Typography variant="body1" fontWeight="bold" mr={1}>
-                  Duração:
-                </Typography>
-                <Typography variant="body1">2h 30min</Typography>
-              </Box>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Typography variant="body1" fontWeight="bold" mr={1}>
-                  Popularidade:
-                </Typography>
-                <Typography variant="body1">{movie.vote_average}</Typography>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid container item xs={3}>
-            <Box>
-              <Typography variant="body1" fontWeight="bold">
-                Genêro: <span>{movie.genre_ids[1]}</span>
+            <Box display="flex" alignItems="center">
+              <Typography variant="body1" mr={3}>
+                <strong>Duração: </strong>2h 30min
               </Typography>
-              <Typography variant="body1" fontWeight="bold">
-                Atores:
+
+              <Typography variant="body1">
+                <strong>Popularidade: </strong>
+                {movie.vote_average}
               </Typography>
             </Box>
-          </Grid>
-          <Grid container item xs={12} mt={4}>
-            <Typography variant="body1" fontWeight="bold">
-              {movie.overview}
-            </Typography>
           </Grid>
           <Grid
-            container
+            item
+            xs={12}
+            sm={4}
+            display="flex"
+            alignItems={{ xs: "center", sm: "flex-start" }}
+            flexDirection={{ xs: "row", sm: "column" }}
+            justifyContent={{ xs: "start", sm: "end" }}
+          >
+            <Typography variant="body1" mr={{ xs: 3, sm: 0 }}>
+              <strong>Genêro: </strong>
+              {movie.genre_ids[1]}
+            </Typography>
+
+            <Typography variant="body1">
+              <strong>Atores: </strong>Tom roland
+            </Typography>
+          </Grid>
+          <Grid container item xs={12} mt={{ xs: 2, sm: 4 }}>
+            <Typography variant="body2">{movie.overview}</Typography>
+          </Grid>
+          <Grid
             item
             xs={12}
             mt={3}
